5ed47ec2bd729dd42ae6d638fb9d438e967f3c7b
ryunayeon <yoonayeon@gmail.com>
2023-04-28 17:33:44 +0900

    final

--- skeleton/movies/urls.py
+++ skeleton/movies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movie_pk>/', views.detail, name='detail'),
     path('recommended/', views.recommended, name='recommended'),
+    path('recommend/', views.recommend, name='recommend'),
 ]
--- skeleton/movies/views.py
+++ skeleton/movies/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_safe
 from .models import Movie, Genre
 from community.models import Review
 from django.db.models import Q
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -29,24 +30,39 @@ def detail(request, movie_pk):
 
 @require_safe
 def recommended(request):
-    lst = []
-
-    # q = Q()
-    # for i in range(len(lst)):
-    #     q.add(pk = i)
     
-
-
-    # genre = Genre.objects.get(pk=12) | Genre.objects.get(pk=14)
-    genre = Genre.objects.filter(Q(pk=12)|Q(pk=14))
-    print(genre)
-    print(genre.all())
-
-
     context = {
         'genre_lst' : Genre.objects.all()
     }
     return render(request, 'movies/recommended.html', context)
+
+
+# new 
+@require_safe
+def recommend(request):
+    my = request.GET
+    # print(my.getlist('lst[]'))
+    lst = my.getlist('lst[]')
+    lst = list(map(int, lst))
+    # print(lst)
+    # 평점순 정렬 리스트
+    vote_lst = []
+
+    movie_join = Movie.objects.prefetch_related('genres')
+    for i in movie_join:
+        flag = False
+        for j in i.genres.all():
+            if j.id in lst:
+                flag = True
+        if flag == True:
+            vote_lst.append((i.poster_path, i.vote_average))
+    vote_lst.sort(key=lambda x:-x[1])
+    # print(vote_lst)
+    
+    context = {
+        'data': vote_lst[:10]
+    }
+    return JsonResponse(context, safe=False)
 
 
 
